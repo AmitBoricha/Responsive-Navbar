3ec9d7a680948773dbba8ea18510090044c47a12
AmitBoricha <borichaamit1@gmail.com>
2023-07-22 00:21:36 +0530

    finished

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-        const toggleBtn = document.querySelector('.toggle_btn')
-        const toggleBtnIcon = document.querySelector('.toggle_btn i')
-        const dropdownMenu = document.querySelector('.dropdown_menu')
-
-        toggleBtn.onclick = function () {
-            dropdownMenu.classList.toggle('open')
-        }
-</script>
